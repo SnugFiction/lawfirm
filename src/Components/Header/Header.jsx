8257a4ba11bbf4429a8074c16767ca00a54231ab
SnugFiction <shoheb.ss987@gmail.com>
2023-09-27 13:55:52 +0530

    Bulding Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ReactComponent as Logo} from "../../Assets/logo.svg"
+
+const Header = () => {
+  return (
+    <div>
+       <Logo/>
+    </div>
+  )
+}
+
+export default Header
